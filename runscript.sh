8e89e1acc9ce48c006b62c7201e89ecc21f6f72c
Grampachampa <grampachampa@gmail.com>
2023-01-28 12:14:04 +0100

    asd

--- runscript.sh
+++ runscript.sh
@@ -7,8 +7,8 @@
 #SBATCH -C Titan
 #SBATCH --gres=gpu:1
 
-# module load cuda10.0/toolkit
-# module load cuDNN/cuda10.0
+# module load cuda10.0/toolkit/10.0.130
+# module load cuDNN/cuda10.0/7.4
 
 
 source $HOME/.bashrc
